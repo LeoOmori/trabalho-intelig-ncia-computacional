41b4138792c83335cf124027a173c23ef26efe18
leoOmori <leonardoomori99@gmail.com>
2022-11-15 21:08:23 -0300

    Adicionado HOG e SIFT como descritores

--- feature.py
+++ feature.py
@@ -2,6 +2,10 @@ from skimage.feature import local_binary_pattern
 from skimage.feature import graycomatrix, graycoprops
 import cv2  
 import numpy as np
+from skimage.feature import SIFT
+from skimage import transform
+from skimage.feature import hog
+from sklearn.decomposition import PCA
 
 class LocalBinaryPatterns:
   def __init__(self, numPoints, radius):
@@ -36,10 +40,28 @@ def glcmExtraction(img):
     # print all features
     print("Contrast: {} {} {} {} {} {}".format(contrast, dissimilarity, homogeneity, energy, correlation, ASM))
 
+def siftExtraction(img):
+  sift = SIFT()
+  ## scale image to 1/3 of original size
+  img2 = transform.rescale(img, 1.0 / 3.0)
+  sift.detect_and_extract(img2)
+  print("SIFT features: {}".format(sift.descriptors))
+
+def hogFeature(img):
+  fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8),
+                    cells_per_block=(2, 2), visualize=True, feature_vector=True)
+  print("HOG features: {}".format(fd))
+
+def pcaReduction(img):
+  pca = PCA()
+  pca.fit(img)
+  print("PCA features: {}".format(pca.singular_values_))
+  return 
+
 def main():
-    img = cv2.imread("dataset_cartas/CF00314_02.bmp")
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    glcmExtraction(img)
+    img = cv2.imread("\database\dataset_cartas\CF00001")
+    print("Image shape: {}".format(img.singular_values_))
+    hogFeature(img)
 
 if __name__=="__main__":
     main()
